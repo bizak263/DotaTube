75c7a95031eda35baa5dbccbfce8e209075ee4fb
bizak263 <bebroyd@gmail.com>
2024-11-08 04:07:43 +0500

    Add files via upload

---
+++ DotaTube/videos-page2.js
@@ -0,0 +1,102 @@
+const videosPage2 = [
+    {
+        id: '456239068_-226781571',
+        title: 'СЕРЕГА ПИРАТ - ЛУЧШИЕ МОМЕНТЫ СТРИМА',
+        channelTitle: 'Серега Пират',
+        thumbnail: 'https://vk.com/images/video-226781571_456239068.jpg',
+        views: '156K просмотров',
+        duration: '22:15',
+        embedUrl: 'https://vk.com/video_ext.php?oid=-226781571&id=456239068&hd=4&autoplay=1',
+        allow: "autoplay; encrypted-media; fullscreen; picture-in-picture; screen-wake-lock;"
+    },
+    {
+        id: '456258020_-203263472',
+        title: 'СЕРЕГА ПИРАТ - НОВЫЙ КОНТЕНТ',
+        channelTitle: 'Серега Пират',
+        thumbnail: 'https://vk.com/images/video-203263472_456258020.jpg',
+        views: '203K просмотров',
+        duration: '25:10',
+        embedUrl: 'https://vk.com/video_ext.php?oid=-203263472&id=456258020&hd=3&autoplay=1',
+        allow: "autoplay; encrypted-media; fullscreen; picture-in-picture; screen-wake-lock;"
+    },
+    {
+        id: '456265345_-204041665',
+        title: 'СЕРЕГА ПИРАТ - ЭПИЧНЫЕ МОМЕНТЫ',
+        channelTitle: 'Серега Пират',
+        thumbnail: 'https://vk.com/images/video-204041665_456265345.jpg',
+        views: '167K просмотров',
+        duration: '20:45',
+        embedUrl: 'https://vk.com/video_ext.php?oid=-204041665&id=456265345&hd=3&autoplay=1',
+        allow: "autoplay; encrypted-media; fullscreen; picture-in-picture; screen-wake-lock;"
+    },
+    {
+        id: '456298233_-200900163',
+        title: 'СЕРЕГА ПИРАТ - ЛУЧШИЕ МОМЕНТЫ НЕДЕЛИ',
+        channelTitle: 'Серега Пират',
+        thumbnail: 'https://vk.com/images/video-200900163_456298233.jpg',
+        views: '289K просмотров',
+        duration: '28:15',
+        embedUrl: 'https://vk.com/video_ext.php?oid=-200900163&id=456298233&hd=3&autoplay=1',
+        allow: "autoplay; encrypted-media; fullscreen; picture-in-picture; screen-wake-lock;"
+    },
+    {
+        id: '456303432_-200900163',
+        title: 'СЕРЕГА ПИРАТ - ИМБА ИГРА',
+        channelTitle: 'Серега Пират',
+        thumbnail: 'https://vk.com/images/video-200900163_456303432.jpg',
+        views: '78K просмотров',
+        duration: '15:20',
+        embedUrl: 'https://vk.com/video_ext.php?oid=-200900163&id=456303432&hd=3&autoplay=1',
+        allow: "autoplay; encrypted-media; fullscreen; picture-in-picture; screen-wake-lock;"
+    },
+    {
+        id: '456268140_-203263472',
+        title: 'СЕРЕГА ПИРАТ - МЕГА КАМБЭК',
+        channelTitle: 'Серега Пират',
+        thumbnail: 'https://vk.com/images/video-203263472_456268140.jpg',
+        views: '245K просмотров',
+        duration: '19:55',
+        embedUrl: 'https://vk.com/video_ext.php?oid=-203263472&id=456268140&hd=3&autoplay=1',
+        allow: "autoplay; encrypted-media; fullscreen; picture-in-picture; screen-wake-lock;"
+    },
+    {
+        id: '456248640_-167803767',
+        title: 'СЕРЕГА ПИРАТ - ЛУЧШИЕ МОМЕНТЫ СТРИМА',
+        channelTitle: 'Серега Пират',
+        thumbnail: 'https://vk.com/images/video-167803767_456248640.jpg',
+        views: '134K просмотров',
+        duration: '16:40',
+        embedUrl: 'https://vk.com/video_ext.php?oid=-167803767&id=456248640&hd=3&autoplay=1',
+        allow: "autoplay; encrypted-media; fullscreen; picture-in-picture; screen-wake-lock;"
+    },
+    {
+        id: '456248641_-167803397',
+        title: 'СЕРЕГА ПИРАТ - ЭПИЧНЫЕ МОМЕНТЫ НЕДЕЛИ',
+        channelTitle: 'Серега Пират',
+        thumbnail: 'https://vk.com/images/video-167803397_456248641.jpg',
+        views: '167K просмотров',
+        duration: '21:30',
+        embedUrl: 'https://vk.com/video_ext.php?oid=-167803397&id=456248641&hd=3&autoplay=1',
+        allow: "autoplay; encrypted-media; fullscreen; picture-in-picture; screen-wake-lock;"
+    },
+    {
+        id: '456286936_-204041665',
+        title: 'СЕРЕГА ПИРАТ - НОВЫЙ ПАТЧ',
+        channelTitle: 'Серега Пират',
+        thumbnail: 'https://vk.com/images/video-204041665_456286936.jpg',
+        views: '198K просмотров',
+        duration: '17:25',
+        embedUrl: 'https://vk.com/video_ext.php?oid=-204041665&id=456286936&hd=4&autoplay=1',
+        allow: "autoplay; encrypted-media; fullscreen; picture-in-picture; screen-wake-lock;"
+    },
+    {
+        id: '456302203_-204041665',
+        title: 'СЕРЕГА ПИРАТ - ЛУЧШИЕ МОМЕНТЫ',
+        channelTitle: 'Серега Пират',
+        thumbnail: 'https://vk.com/images/video-204041665_456302203.jpg',
+        views: '276K просмотров',
+        duration: '23:40',
+        embedUrl: 'https://vk.com/video_ext.php?oid=-204041665&id=456302203&hd=3&autoplay=1',
+        allow: "autoplay; encrypted-media; fullscreen; picture-in-picture; screen-wake-lock;"
+    }
+]; 
